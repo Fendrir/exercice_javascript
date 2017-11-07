4531c218b6ee551ccff47e5396682eb875f55df1
Fendrir <papao1@hotmail.fr>
2017-11-07 11:58:30 +0100

    ajout d'une nouvelle façon de trier en décroissant cette fois avec un double boucle

--- app.js
+++ app.js
@@ -1,24 +1,98 @@
-function test1(z)
-{
-   z = z + "suffixe";
-}
+// function test1(z)
+// {
+//    z = z + "suffixe";
+// }
 
-var x = "démo";
-test1(x);
+// var x = "démo";
+// test1(x);
 
-console.log(x);
+// console.log(x);
 
 
 
 
 // _____________________________________________________________
 
-function test2(a)
-{
-   a.push("ajouté");
-}
+// function test2(a)
+// {
+//    a.push("ajouté");
+// }
 
-x = ["un", "deux", "trois"];
-test2(x);
+// x = ["un", "deux", "trois"];
+// test2(x);
 
-console.log(x);
+// console.log(x);
+
+// ________________________ faire un tri des chiffres ____________________________________
+
+
+// tab = [1,4,2,5,1,5,3,20,15,50,1,5,-2,-20];
+
+
+// tab.sort(function(i, j) {
+
+//     if (i < j) {
+            
+//         return -1;
+            
+//     } else if (i > j) {
+            
+//         return 1;
+            
+//     } else {
+            
+//         return 0;
+            
+//     }
+
+// });
+
+
+
+// alert(tab);
+
+
+// ________________________________ essayer autre façon ___________________________________
+
+
+// tab = [1,4,2,5,1,5,3,20,15,50,1,5,-2,-20];
+
+
+var func = function(a,b){
+    return a>b ;
+ };
+
+function tri(tab,func){
+
+    for (var i = 0; i < tab.length; i++) {
+
+        for (var j = i+1; j < tab.length; j++){
+
+            if(func(tab[j],tab[i])){
+
+                var tabTri = tab[j];
+                tab[j]=tab[i];
+                tab[i]=tabTri;
+
+            }
+
+        }
+
+        
+    }
+    
+    return tab;
+
+};
+
+console.log(tri([1,4,2,5,1,5,3,20,15,50,1,5,-2,-20],function(a,b){
+
+    return a>b;
+}));
+
+alert(tri([1,4,2,5,1,5,3,20,15,50,1,5,-2,-20],function(a,b){
+
+    return a>b;
+}));
+
+ 
